55e9207e3183a5c02e539c9d3387f6ce3569c8f6
James John McGuire <jamesjohnmcguire@gmail.com>
2016-10-16 13:12:24 +0000

    New base classes for collections

---
+++ IDatabaseCollection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalZenWorks.Common.DatabaseLibrary
+{
+	interface IDatabaseCollection : IDisposable
+	{
+		int GetIdFromName(string name);
+	}
+}
