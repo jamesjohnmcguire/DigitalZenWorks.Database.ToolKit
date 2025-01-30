f46e3290fa03523ee0601aedfdcc1db2c01724d7
James John McGuire <jamesjohnmcguire@gmail.com>
2025-01-30 11:08:31 +0900

    Add Database Connection Class

---
+++ DatabaseLibraryNet/DatabaseConnection.cs
@@ -0,0 +1,15 @@
+﻿/////////////////////////////////////////////////////////////////////////////
+// <copyright file="DatabaseConnection.cs" company="James John McGuire">
+// Copyright © 2006 - 2025 James John McGuire. All Rights Reserved.
+// </copyright>
+/////////////////////////////////////////////////////////////////////////////
+
+namespace DigitalZenWorks.Database.ToolKit
+{
+	/// <summary>
+	/// The database connection class.
+	/// </summary>
+	public class DatabaseConnection
+	{
+	}
+}
