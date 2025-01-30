de098f24d2eeeacf8081212a9d01ac0ddbf98074
James John McGuire <jamesjohnmcguire@gmail.com>
2025-01-30 11:22:47 +0900

    Implement IDisposable

--- DatabaseLibraryNet/DatabaseConnection.cs
+++ DatabaseLibraryNet/DatabaseConnection.cs
@@ -4,12 +4,14 @@
 // </copyright>
 /////////////////////////////////////////////////////////////////////////////
 
+using System;
+
 namespace DigitalZenWorks.Database.ToolKit
 {
 	/// <summary>
 	/// The database connection class.
 	/// </summary>
-	public class DatabaseConnection
+	public class DatabaseConnection : IDisposable
 	{
 		private string connectionString;
 
@@ -22,6 +24,27 @@ namespace DigitalZenWorks.Database.ToolKit
 		public DatabaseConnection(string connectionString)
 		{
 			this.connectionString = connectionString;
+		}
+
+		/// <summary>
+		/// The dispose method.
+		/// </summary>
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+		/// <summary>
+		/// The dispose method.
+		/// </summary>
+		/// <param name="disposing">Indicates whether it is
+		/// currently disposing.</param>
+		protected virtual void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+			}
 		}
 	}
 }
