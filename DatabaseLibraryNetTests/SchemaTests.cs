e8a3de2f6c963e0bee55ea8a9314745ae386ba4f
James John McGuire <jamesjohnmcguire@gmail.com>
2025-06-15 13:24:23 +0900

    Add New Test File

---
+++ DatabaseLibraryNetTests/SchemaTests.cs
@@ -0,0 +1,17 @@
+﻿/////////////////////////////////////////////////////////////////////////////
+// Copyright @ 2006 - 2025 by James John McGuire
+// All rights reserved.
+/////////////////////////////////////////////////////////////////////////////
+
+using NUnit.Framework;
+
+namespace DigitalZenWorks.Database.ToolKit.Tests
+{
+	/// <summary>
+	/// Schema tests class.
+	/// </summary>
+	[TestFixture]
+	internal sealed class SchemaTests
+	{
+	}
+}
