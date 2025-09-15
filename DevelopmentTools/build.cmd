cc30321f79b8a06bbf3cc792e671588bef685acb
James John McGuire <jamesjohnmcguire@gmail.com>
2025-09-15 19:42:51 +0900

    Move Files to More Standardized Locations

--- DevelopmentTools/build.cmd
+++ DevelopmentTools/build.cmd
@@ -3,25 +3,26 @@ REM %2 - Version (such as 1.0.0.5)
 REM %3 - API key
 
 CD %~dp0
-CD DatabaseLibraryNet
-
-IF "%1"=="publish" GOTO publish
+CD ..\DatabaseLibraryNet
 
 :default
-dotnet build --configuration Release
+dotnet build --configuration Release -p:PlatformTarget=AnyCPU -p:Prefer32Bit=false
 
+IF "%1"=="publish" GOTO publish
 GOTO end
 
 :publish
-
 if "%~2"=="" GOTO error1
 if "%~3"=="" GOTO error2
 
-msbuild -property:Configuration=Release -restore -target:rebuild;pack DigitalZenWorks.Database.ToolKit.csproj
+::msbuild -property:Configuration=Release -restore -target:rebuild;pack DigitalZenWorks.Database.ToolKit.csproj
+dotnet pack --configuration Release --output nupkg -p:IncludeSource=true -p:IncludeSymbols=true
 
-CD bin\Release
+CD nupkg
 
 nuget push DigitalZenWorks.Database.ToolKit.%2.nupkg %3 -Source https://api.nuget.org/v3/index.json
+
+CD ..
 GOTO end
 
 :error1
@@ -32,4 +33,3 @@ GOTO end
 ECHO No API key specified
 
 :end
-cd ..
