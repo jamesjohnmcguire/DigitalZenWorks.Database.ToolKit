e1544534dac18e8a9d33539c04c23c48a2fecc3a
James John McGuire <jamesjohnmcguire@gmail.com>
2025-09-16 19:26:36 +0900

    Add Platform Neutral Schema Class

---
+++ DatabaseLibraryNet/Schema.cs
@@ -0,0 +1,20 @@
+﻿/////////////////////////////////////////////////////////////////////////////
+// <copyright file="Schema.cs" company="James John McGuire">
+// Copyright © 2006 - 2025 James John McGuire. All Rights Reserved.
+// </copyright>
+/////////////////////////////////////////////////////////////////////////////
+
+namespace DigitalZenWorks.Database.ToolKit
+{
+	using System;
+	using System.Data;
+	using System.Globalization;
+
+	/// Class <c>Schema.</c>
+	/// <summary>
+	/// Represents a database schema.
+	/// </summary>
+	public class Schema
+	{
+	}
+}
