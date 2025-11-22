7818775ae70f52a03ca0cfb8349e4b72285b2f1a
James John McGuire <jamesjohnmcguire@gmail.com>
2025-11-22 12:49:15 +0900

    Add Test Infrastructure Support

--- DatabaseLibraryNetTests/SchemaTests.cs
+++ DatabaseLibraryNetTests/SchemaTests.cs
@@ -4,6 +4,13 @@
 
 namespace DigitalZenWorks.Database.ToolKit.Tests
 {
+	using System;
+	using System.Collections.Generic;
+	using System.Data;
+	using System.Data.SQLite;
+	using System.Globalization;
+	using System.IO;
+	using System.Linq;
 	using NUnit.Framework;
 
 	/// <summary>
@@ -12,5 +19,57 @@ namespace DigitalZenWorks.Database.ToolKit.Tests
 	[TestFixture]
 	internal sealed class SchemaTests
 	{
+		/// <summary>
+		/// database storage object.
+		/// </summary>
+		private DataStorage database;
+		private string dataSource;
+
+		/// <summary>
+		/// One time set up method.
+		/// </summary>
+		[OneTimeSetUp]
+		public void OneTimeSetUp()
+		{
+			dataSource = GetTestDatabasePath();
+
+			SQLiteConnection.CreateFile(dataSource);
+
+			string connectionBase = "Data Source={0};Version=3;" +
+				"DateTimeFormat=InvariantCulture";
+
+			string connectionString = string.Format(
+				CultureInfo.InvariantCulture,
+				connectionBase,
+				dataSource);
+
+			database = new DataStorage(DatabaseType.SQLite, connectionString);
+		}
+
+		/// <summary>
+		/// function that is called when all tests are completed.
+		/// </summary>
+		[OneTimeTearDown]
+		public void OneTimeTearDown()
+		{
+			if (database != null)
+			{
+				database.Close();
+				database.Shutdown();
+			}
+
+			File.Delete(dataSource);
+		}
+
+		private static string GetTestDatabasePath()
+		{
+			string fileName = Path.GetTempFileName();
+
+			// A 0 byte sized file is created.  Need to remove it.
+			File.Delete(fileName);
+			string databasePath = Path.ChangeExtension(fileName, ".db");
+
+			return databasePath;
+		}
 	}
 }
