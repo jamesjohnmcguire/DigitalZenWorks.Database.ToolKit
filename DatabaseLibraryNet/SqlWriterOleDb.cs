4c81638b8f08f58b552fe9da5a81e29616a7e41b
James John McGuire <jamesjohnmcguire@gmail.com>
2025-12-02 21:45:41 +0900

    Add SqlWriterOleDb Class

---
+++ DatabaseLibraryNet/SqlWriterOleDb.cs
@@ -0,0 +1,15 @@
+﻿/////////////////////////////////////////////////////////////////////////////
+// <copyright file="SqlWriterOleDb.cs" company="James John McGuire">
+// Copyright © 2006 - 2025 James John McGuire. All Rights Reserved.
+// </copyright>
+/////////////////////////////////////////////////////////////////////////////
+
+namespace DigitalZenWorks.Database.ToolKit
+{
+	/// <summary>
+	/// SQL writer helper for OleDb class.
+	/// </summary>
+	public class SqlWriterOleDb : SqlWriter
+	{
+	}
+}
